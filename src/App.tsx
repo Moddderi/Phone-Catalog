5ecb6fdb475d4384b1b0c6d27178789981243842
Oleh Kirich <olehkirich@MacBook-Air-Oleh.local>
2026-01-17 18:49:11 +0200

    End prooject

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
-import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './Pages/HomePage/HomePage';
+import { NotFoundPage } from './Pages/NotFoundPage/NotFoundPage';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import './styles/global.scss';
+// eslint-disable-next-line
+import { AddAndFavoritesProvider } from './components/context/AddAndFavoritesContext';
+import { PathProvider } from './components/context/PathContext';
+import { ProductCategoryPage } from './Pages/ProductCategoryPage';
+import { CartPage } from './Pages/CartPage/CartPage';
+import { FavoritesPage } from './Pages/FavoritesPage/FavoritesPage';
+import { ProductInfoPage } from './Pages/ProductInfoPage/ProductInfoPage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export const App = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <AddAndFavoritesProvider>
+      <PathProvider>
+        <div className="app">
+          <Header />
+
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+
+              <Route path=":category">
+                <Route index element={<ProductCategoryPage />} />
+
+                <Route path=":itemId" element={<ProductInfoPage />} />
+              </Route>
+
+              <Route path="/favorites" element={<FavoritesPage />} />
+
+              <Route path="/cart" element={<CartPage />} />
+
+              <Route
+                path="/product-not-found"
+                element={<NotFoundPage type="page" />}
+              />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </PathProvider>
+    </AddAndFavoritesProvider>
   );
 };
